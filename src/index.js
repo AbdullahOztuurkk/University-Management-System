7b64848b73be1be052fcde8109d8767eabca272a
Taha Doğan <mtahadogan.biz@outlook.com>
2021-05-16 04:31:27 +0300

    feat: sync functions has been written more readable in main-index.js

--- src/index.js
+++ src/index.js
@@ -15,18 +15,26 @@ if (constants.NODE_ENV === 'development') {
     // Morgan logger
     app.use(morgan('tiny'));
 
-    // Sync database columns
-    const faculty = require('./models/faculty');
-    faculty.sync({ force: true });
+    // // Sync database columns
+    // const faculty = require('./models/faculty');
+    // faculty.sync({ force: true });
 
-    const department = require('./models/department');
-    department.sync({ force: true });
+    // const department = require('./models/department');
+    // department.sync({ force: true });
 
-    const lesson = require('./models/lesson');
-    lesson.sync({ force: true });
+    // const lesson = require('./models/lesson');
+    // lesson.sync({ force: true });
 
-    const user = require('./models/user');
-    user.sync({ force: true });
+    // const user = require('./models/user');
+    // user.sync({ force: true });
+
+    require('./models/faculty').sync({ force: true });
+
+    require('./models/department').sync({ force: true });
+
+    require('./models/lesson').sync({ force: true });
+
+    require('./models/user').sync({ force: true });
 
     console.log(`All models are synced`.green.bold);
 }
