28b2f82e1fe4ae299a35c14f7e4b1ec2387181f4
Taha Doğan <mtahadogan.biz@outlook.com>
2021-05-17 05:02:51 +0300

    refactor: old sequelize configs are removed from into the index.js

--- src/index.js
+++ src/index.js
@@ -2,20 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const constants = require('./constants/constants');
 
-const models = require('./models');
-
 var app = express();
 
 app.use(express.json())
-
-// db connection
-
-//Routes Settings
-// app.use('/api/users', UserRoutes);
-// app.use('/api/lecturers', LecturerRoutes);
-// app.use('/api/lessons', LessonRoutes);
-// app.use('/api/lecturerlessons', LecturerLessonRoutes);
-// app.use('/api/grades', GradeRoutes);
 
 var port = constants.PORT || 5000;
 
