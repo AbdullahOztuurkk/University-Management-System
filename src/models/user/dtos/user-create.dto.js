e5903b8381f7bb19b91effa8df540ac934cf92a9
Taha Doğan <mtahadogan.biz@outlook.com>
2021-05-20 01:30:16 +0300

    refactor: created dates and sesion column in user-lessons table , refactor: prepared code template edited in user-create-dto.js

--- src/models/user/dtos/user-create.dto.js
+++ src/models/user/dtos/user-create.dto.js
@@ -1,7 +1,23 @@
 const joi = require('joi');
+const ErrorResponse = require('../../../utils/ErrorResponse');
+const options = require('../../dto-options');
 
-const userCreateDto = {
-    body: joi.object(
-        // Code here...
-    ),
+function userCreateDto(req, res, next) {
+
+    const schema = {
+        body: joi.object({
+            // Code here...
+        }),
+    }
+
+    const { error, value } = schema.body.validate(req.body, options);
+
+    if (error) {
+        next(new ErrorResponse(`${error.message}`, 400));
+    }
+    req.body = value;
+    next();
+
 }
+
+module.exports = userCreateDto;
