d65949df91568c482cd4b14a863b95ccae8ee414
Abdullah Öztürk <abdullahozturk99@gmail.com>
2021-05-21 11:10:02 +0300

    feat: Lesson controller has been redesigned for revision

--- src/controllers/lesson.controller.js
+++ src/controllers/lesson.controller.js
@@ -4,21 +4,109 @@ const { Lesson } = require('../models/lesson/lesson.model');
 const ErrorResponse = require('../utils/ErrorResponse');
 
 exports.getById = asyncHandler(async (req, res, next) => {
-    // Code here.
+    const id = parseInt(req.params.id);
+
+    const lesson = await client.lesson.findUnique({
+        where: {
+            id: id,
+        },
+        select: {
+            id: true,
+            name: true,
+            code:true,
+            credit:true,
+            department: {
+                select: {
+                    id: true,
+                    name: true,
+                    slugifyName: true,
+                },
+            },
+        },
+
+    });
+
+    res.status(200).json({
+        success: true,
+        data: lesson,
+    });
+
 });
 
 exports.getAll = asyncHandler(async (req, res, next) => {
-    // Code here.
+    
+    const lessons = await client.lesson.findMany({
+        select: {
+            id: true,
+            name: true,
+            code:true,
+            credit:true,
+            departments: {
+                select: {
+                    id: true,
+                    name: true,
+                    slugifyName: true,
+                },
+            },
+        },
+    });
+
+
+    res.status(200).json({
+        success: true,
+        data: lessons,
+        count:lessons.count
+    });
+
 });
 
 exports.create = asyncHandler(async (req, res, next) => {
-    // Code here.
+    
+    const lessonModel = new Lesson(req.body);
+
+    const created = await client.lesson.create({
+        data: lessonModel,
+    });
+
+    res.status(200).json({
+        success: true,
+        data: created,
+    });
+
 });
 
 exports.delete = asyncHandler(async (req, res, next) => {
-    // Code here.
+    
+    const id = parseInt(req.params.id);
+
+    const deleted = await client.lesson.delete({
+        where: {
+            id: id,
+        },
+    });
+
+    res.status(200).json({
+        success: true,
+        message: `${deleted.name} başarıyla silindi.`,
+    })
 });
 
 exports.update = asyncHandler(async (req, res, next) => {
-    // Code here.
+    
+    const id = parseInt(req.params.id);
+
+    const lessonModel = new Lesson(req.body);
+
+    const updated = await client.faculty.update({
+        where: {
+            id: id,
+        },
+        data: lessonModel,
+    });
+
+    res.status(200).json({
+        success: true,
+        data: updated,
+    });
+
 });
