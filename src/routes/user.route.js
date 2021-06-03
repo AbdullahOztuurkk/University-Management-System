d606e1edc866eb8d794c8930a49b8475dff792ec
Abdullah Öztürk <abdullahozturk99@gmail.com>
2021-06-03 22:15:43 +0300

    fix: changed route priority for prevent complication.

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -21,17 +21,22 @@ const router = express.Router();
 router.use(jwtAuthentication);
 
 router
-	.route("/:id")
-	.get(getById)
-	.patch(authorize("ADMIN"), updateById)
-	.delete(authorize("ADMIN"), deleteById);
-router.route("/").post(authorize("ADMIN"), createUserLessonDto, create).get(getAll);
-router.route("/teachers", getTeachers);
-router.route("/students", getStudents);
-router
 	.route("/lessons")
 	.get(authorize("STUDENT"), getJoinedLessons)
 	.post(authorize("STUDENT"), joinOpenedLesson)
 	.delete(authorize("STUDENT"), leaveOpenedLesson);
+
+router.route("/teachers", getTeachers);
+router.route("/students", getStudents);
+
+router
+	.route("/:id")
+	.get(getById)
+	.patch(authorize("ADMIN"), updateById)
+	.delete(authorize("ADMIN"), deleteById);
+router
+	.route("/")
+	.post(authorize("ADMIN"), createUserLessonDto, create)
+	.get(getAll);
 
 module.exports = router;
