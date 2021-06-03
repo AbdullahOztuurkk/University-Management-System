514acc9a8530ee3976e711519e055801d525acc8
Taha Doğan <mtahadogan.biz@outlook.com>
2021-06-03 15:53:20 +0300

    fix: slugify removed from constructor

--- src/models/faculty/faculty.model.js
+++ src/models/faculty/faculty.model.js
@@ -3,8 +3,6 @@ const { default: slugify } = require("slugify");
 exports.Faculty = class {
     constructor(object) {
         Object.assign(this, object);
-
-        this.slugifyName = slugify(this.name, { replacement: '-', lower: true });
     }
     id;
     name;
