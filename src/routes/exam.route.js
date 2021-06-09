afd7f2e8eabecda732d984f00512facd8454855c
Abdullah Öztürk <abdullahozturk99@gmail.com>
2021-06-09 21:19:25 +0300

    feat: me method has been created in exam route.

--- src/routes/exam.route.js
+++ src/routes/exam.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAll,create,deleteById,getById,updateById} = require('../controllers/exam.controller');
+const { getAll,create,deleteById,getById,updateById, getAllStudentExams} = require('../controllers/exam.controller');
 const { jwtAuthentication, authorize } = require('../middleware/auth');
 const createDto = require('../models/exam/dtos/exam-create.dto');
 
@@ -10,12 +10,14 @@ const router = express.Router();
 
 router.use(jwtAuthentication);
 
+router.route('/me').get(getAll);
+
 router.route('/:id')
     .get(getById)
     .patch(authorize('TEACHER','ADMIN'), updateById)
     .delete(authorize('TEACHER','ADMIN'),deleteById);
 router.route('/')
     .post(authorize('TEACHER','ADMIN'),createDto, create)
-    .get(getAll);
+    .get(getAllStudentExams);
 
 module.exports = router;
