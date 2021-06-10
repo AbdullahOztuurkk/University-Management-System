8952acb606e4e6ac0c9c0709b88321998d0627b2
Abdullah Öztürk <abdullahozturk99@gmail.com>
2021-06-11 00:41:34 +0300

    feat: added get methods and fixed some problems about complicated methods.

--- src/controllers/exam.controller.js
+++ src/controllers/exam.controller.js
@@ -7,24 +7,27 @@ exports.getById = asyncHandler(async (req, res, next) => {
     const userId = parseInt(req.user.id);
     const userlessonId = parseInt(req.params.id);
 
-    const exam = await client.Exam.findUnique({
+    const exam = await client.exam.findUnique({
         select: {
             type:true,
             score:true,
             announcementDate:true,
-            userLesson:{
-                user:{
-                    firstName:true,
-                    lastName:true,
+            UserLesson:{
+                select:{
                     id:true,
-                },
-                lesson:{
-                    name:true,
-                    code:true,
-                    credit:true
-                },
-                where:{
-                    id:userId,
+                    user:{
+                        select:{
+                            firstName:true,
+                            lastName:true,
+                        }
+                    },
+                    lesson:{
+                        select:{
+                            name:true,
+                            code:true,
+                            credit:true
+                        }
+                    },
                 }
             },
         },
@@ -51,6 +54,7 @@ exports.getAll = asyncHandler(async (req, res, next) => {
 
     const exams = await client.exam.findMany({
         select: {
+            id:true,
             type:true,
             score:true,
             announcementDate:true,
@@ -88,16 +92,73 @@ exports.getAll = asyncHandler(async (req, res, next) => {
     });
 });
 
+exports.getAllStudentExams = asyncHandler(async (req, res, next) => {
+
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    console.log("BU METOD ÇALIŞIYOR")
+
+    const exams = await client.exam.findMany({
+        select: {
+            id:true,
+            type:true,
+            score:true,
+            announcementDate:true,
+            UserLesson:{
+                select:{
+                    user:{
+                        select:{
+                            firstName:true,
+                            lastName:true,
+                        },
+                    },
+                    lesson:{
+                        select:{
+                            name:true,
+                            code:true,
+                            credit:true
+                        }
+                    }
+                },
+            },
+        },
+        skip: skip,
+        take: limit,
+    });
+
+    // TODO: pagination headers
+
+    res.status(200).json({
+        success: true,
+        data: exams,
+    });
+});
+
 exports.create = asyncHandler(async (req, res, next) => {
-    //Code Here
+    const examModel=new Exam(req.body);
+    //examModel.userLessonId = parseInt(req.params.id);
+    console.log(req.body);
+
+    const created = await client.exam.create({
+        data:examModel
+    });
+
+    res.status(201).json({
+        success:true,
+        data:created
+    })
 });
 
 exports.updateById = asyncHandler(async (req, res, next) => {
    
     const examId = parseInt(req.params.id);
     const examModel = new Exam(req.body);
+    examModel.id=examId;
+    console.log(examId)
 
-    const updated = await client.Exam.update({
+    const updated = await client.exam.update({
         where: {
             id: examId,
         },
@@ -115,7 +176,7 @@ exports.deleteById = asyncHandler(async (req, res, next) => {
     
     const examId = parseInt(req.params.id);
 
-    const deleted = await client.Exam.delete({
+    const deleted = await client.exam.delete({
         where: {
             id: examId,
         },
@@ -123,7 +184,7 @@ exports.deleteById = asyncHandler(async (req, res, next) => {
 
     res.status(200).json({
         success: true,
-        message: `${deleted.name} başarıyla silindi.`,
+        message: `${deleted.type} başarıyla silindi.`,
     })
 
 });
